14c89a53ec5d1afd996f86e2107e1a7457407077
Rabs <karkirabu@gmail.com>
2021-11-23 22:43:04 +0545

    error-handling-updated

--- NATO-alphabet-project/main.py
+++ NATO-alphabet-project/main.py
@@ -1,12 +1,20 @@
 
 import pandas
- 
+
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict={row.letter:row.code for (index,row) in data.iterrows()}
 #ask user for input and convert it into uppercase
 
-word=input("Enter a word: \n" ).upper()
-output_list=[phonetic_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetic():
+    word=input("Enter a word: \n" ).upper()
+    try:
+        output_list=[phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry only letters in the input are accepted.")
+        generate_phonetic()
+    else:        
+        print(output_list)
+
+generate_phonetic()      
